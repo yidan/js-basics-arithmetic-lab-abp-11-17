8e7a1ca451d3ce3b4b0da7851eaf826eca9c78af
Yidan Wang <wangyidan621@gmail.com>
2017-11-11 13:49:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,6 @@ ageIsValid = Number.isInteger(currentAge);
 
 randomNumber= Math.random()*20;
 
-flooredRandomInteger = Math.floor(randomNumber);
+randomInteger = Math.floor(randomNumber);
 
+randomUserID = Math.ceiling(Math.random(20)) + 1000000000;
